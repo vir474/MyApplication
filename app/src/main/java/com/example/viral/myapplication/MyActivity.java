a8b19c3f59a19bbd4ebde285be411a0c5aff3722
vir474 <viralintwala_16591@hotmail.com>
2016-04-28 23:39:17 -0400

    Activity Cycle Toasts

--- app/src/main/java/com/example/viral/myapplication/MyActivity.java
+++ app/src/main/java/com/example/viral/myapplication/MyActivity.java
@@ -1,10 +1,12 @@
 package com.example.viral.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -14,6 +16,78 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        Context context = getApplicationContext();
+        CharSequence text = "On Create Called!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Context context = getApplicationContext();
+        CharSequence text = "On Stop Called!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Context context = getApplicationContext();
+        CharSequence text = "On Destroy Called!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Context context = getApplicationContext();
+        CharSequence text = "On Back Pressed Called!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Context context = getApplicationContext();
+        CharSequence text = "On Pause Called!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Context context = getApplicationContext();
+        CharSequence text = "On Resume Called!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Context context = getApplicationContext();
+        CharSequence text = "On Start Called!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     public void sendMessage(View view) {
